50abb2f8810f7dd5c341c6f82be8c705a19c11cd
1430905 <jocool513@hotmail.com>
2016-02-03 07:49:06 -0500

    Test1
    Color Change

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -21,7 +21,7 @@ public class Main extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 0, 1);
                 
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
