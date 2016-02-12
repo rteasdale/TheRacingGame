49b8aca1849f0c1165745a2b162f37b866bd651d
rteasdale <rosy.teasdale@gmail.com>
2016-02-12 12:41:36 -0500

    update

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,13 +1,13 @@
 package com.mygdx.game.desktop;
 
+import Test.Test2;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import Test.Test2;
-import Test.Test1;
+import com.mygdx.game.TheRacingGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
             LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-            new LwjglApplication(new Test1(), cfg);
+        new LwjglApplication(new Test2(), cfg);
 	}
 }
