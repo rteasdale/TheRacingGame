1ed7dbdf638433293fa2f8758a71fb11ccec43a8
1430905 <jocool513@hotmail.com>
2016-02-15 15:20:45 -0500

    Added Car PNGs

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,6 +8,6 @@ import Test.Test1;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
             LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-            new LwjglApplication(new Test1(), cfg);
+            new LwjglApplication(new Test2(), cfg);
 	}
 }
