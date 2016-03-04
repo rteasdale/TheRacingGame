1f8fe6434e895414d115876b45f32b883a915891
rteasdale <rosy.teasdale@gmail.com>
2016-03-04 14:20:02 -0500

    update

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -16,11 +16,33 @@ public class DesktopLauncher {
     public static void main (String[] arg) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+        cfg.title = RacingGame.TITLE;
+        cfg.width = RacingGame.V_WIDTH;
+        cfg.height = RacingGame.V_HEIGHT;
+        
+        new LwjglApplication(new Test2(), cfg);
+
+        new LwjglApplication(new RacingGame(), cfg);
+
+=======
+>>>>>>> origin/master
+>>>>>>> Stashed changes
         cfg.title = Game.TITLE;
         cfg.width = Game.V_WIDTH*Game.SCALE;
         cfg.height = Game.V_HEIGHT*Game.SCALE;
         
+<<<<<<< Updated upstream
         new LwjglApplication(new CarTest(), cfg);
+=======
+<<<<<<< HEAD
+        new LwjglApplication(new Test2(), cfg);
+=======
+        new LwjglApplication(new CarTest(), cfg);
+>>>>>>> origin/master
+>>>>>>> Stashed changes
 
     }
 }
