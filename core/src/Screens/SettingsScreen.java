fb625e5f14de8d2fa14071d5464b27ff121407e9
rteasdale <rosy.teasdale@gmail.com>
2016-03-27 12:30:31 -0400

    Menu visuals
    - menu visuals done yet

--- core/src/Screens/SettingsScreen.java
+++ core/src/Screens/SettingsScreen.java
@@ -5,25 +5,44 @@
  */
 package Screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.RacingGame;
 
 /**
  *
  * @author ROSY
  */
-public class SettingsScreen implements Screen {
+public class SettingsScreen implements Screen, InputProcessor {
     private final RacingGame game;
     private Stage stage;
-    private TextureRegion bgTexture;
+    private BitmapFont font;
+    private Skin skin;
+    private Skin check_skin;
     
+    private TextureAtlas atlas;
+  
     private int musicVol;
     private int SFXVol;
     
@@ -33,51 +52,180 @@ public class SettingsScreen implements Screen {
     private Label controlsLbl;
     private Label SFXLbl;
     private Label musicLbl;
-   
-    private CheckBox controlsCheck;
     
+    private ImageButton OKButton;
+    private ImageButton ASDWBtn;
+    private ImageButton arrowsBtn;
+    private ImageButton.ImageButtonStyle OK_style;    
+    private Label.LabelStyle lbl_style;
+    private ImageButton.ImageButtonStyle ASDW_style;
+    private ImageButton.ImageButtonStyle arrows_style;
+   
+    private CheckBox controls1Check;
+    private CheckBox controls2Check;
+    private ButtonGroup<CheckBox> buttonGroup;
+    private CheckBox.CheckBoxStyle check_style;
+
     private Slider SFXVolume;
     private Slider musicVolume;
+    private Slider.SliderStyle slider_style;
     
-    private TextButton OKButton;
+
     
     public SettingsScreen(RacingGame game) {
         this.game = game;
-    }
+        stage = new Stage();
+        font = new BitmapFont(Gdx.files.internal("menu/button_font.fnt"), Gdx.files.internal("menu/button_font.png"),false);
+        skin = new Skin(new TextureAtlas(Gdx.files.internal("menu/menubtns_atlas.txt")));
+        
+        atlas = new TextureAtlas(Gdx.files.internal("menu/settings_atlas.txt"));
+        check_skin = new Skin(atlas);       
+        
+        buttonGroup = new ButtonGroup<CheckBox>();
+        buttonGroup.setMaxCheckCount(1);
+        buttonGroup.setMinCheckCount(1);
+        buttonGroup.setUncheckLast(true);
+        
+        /** Styles */
+        lbl_style = new Label.LabelStyle(font, Color.WHITE);
+        OK_style = new ImageButton.ImageButtonStyle(skin.getDrawable("OK_button"), null, null, null, null, null);
+        //ASDW_style = new ImageButton.ImageButtonStyle(check_skin.getDrawable("ASDW_controls"), check_skin.getDrawable("ASDW_controls_chosen"), null, null, null, null);
+        //arrows_style = new ImageButton.ImageButtonStyle(check_skin.getDrawable("arrowkeys_controls"), check_skin.getDrawable("arrowkeys_controls_chosen"), null, null, null, null);
 
+        check_style = new CheckBox.CheckBoxStyle();
+        check_style.font = font;
+        check_style.fontColor = new Color(Color.WHITE);
+        check_style.checkboxOff = check_skin.getDrawable("checkbox");
+        check_style.checkboxOn = check_skin.getDrawable("checkbox2");
+        
+        slider_style = new Slider.SliderStyle(check_skin.getDrawable("slider"), check_skin.getDrawable("knob"));
+    }
+        
     @Override
     public void show() {
+        OKButton = new ImageButton(OK_style);
+        OKButton.setPosition(1064, 24);
+        
+        title = new Image(new Texture(Gdx.files.internal("menu/settings_title.png")));
+        title.setPosition(376, 624);
+        
+        /** Labels */
+        controlsLbl = new Label("CONTROLS", lbl_style);
+        controlsLbl.setPosition(200, 550);
+        SFXLbl = new Label("SFX", lbl_style);
+        SFXLbl.setPosition(200, 250);
+        musicLbl = new Label("MUSIC", lbl_style);
+        musicLbl.setPosition(200, 150);
+        
+//        ASDWBtn = new ImageButton(ASDW_style);
+//        ASDWBtn.setPosition(100, 100);
+//        arrowsBtn = new ImageButton(arrows_style);
+//        arrowsBtn.setPosition(200, 200);
+        
+        /* Check box*/
+        controls1Check = new CheckBox("AWSD", check_style);
+        controls1Check.setPosition(210, 500);
+        controls2Check = new CheckBox("UP-DOWN-RIGHT-LEFT", check_style);
+        controls2Check.setPosition(410, 500);
+        
+        /**Slider*/
+        SFXVolume = new Slider(0, 10, 4, false, slider_style);
+        SFXVolume.setPosition(300, 100);
+        musicVolume = new Slider(0, 10, 4, false, slider_style);
+        musicVolume.setPosition(300, 50);
 
+        stage.addActor(SFXVolume);
+        stage.addActor(musicVolume);
+        stage.addActor(controls2Check);
+        stage.addActor(controls1Check);
+        stage.addActor(OKButton);
+        stage.addActor(musicLbl);
+        stage.addActor(SFXLbl);
+        stage.addActor(controlsLbl);
+        stage.addActor(title);
+        
+        controls1Check.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeListener.ChangeEvent ce, Actor actor) {
+                
+            }
+        });
+        
     }
 
     @Override
     public void render(float f) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Gdx.gl.glClearColor(3/255f,13/255f,128/255f,0); //set background color
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        stage.act(f);
+        stage.draw();             
     }
 
     @Override
     public void resize(int i, int i1) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void pause() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void resume() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void hide() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void dispose() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        game.dispose();
+        skin.dispose();
+        check_skin.dispose();
+        stage.dispose();
+        font.dispose();
+        
+    }
+
+    @Override
+    public boolean keyDown(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int i, int i1, int i2) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int i) {
+        return false;
     }
     
 }
