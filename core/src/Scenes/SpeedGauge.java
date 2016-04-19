9ebd18d0dd732e1ebd4309374f26256a83454f84
rteasdale <rosy.teasdale@gmail.com>
2016-04-19 12:24:22 -0400

    HUD updated

---
+++ core/src/Scenes/SpeedGauge.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Scenes;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+/**
+ *
+ * @author ROSY
+ */
+public class SpeedGauge extends Actor {
+    private Image needle;
+    private Image gauge;
+    
+    public SpeedGauge() {
+        needle = new Image(new Texture("HUD/speed_needle.png"));
+        gauge = new Image(new Texture("HUD/speedgauge.png"));
+    }
+    
+}
